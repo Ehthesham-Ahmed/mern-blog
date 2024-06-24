c7383958d9f01e58708e41e3b7886754b63a7d76
Ehthesham-Ahmed <117994804+Ehthesham-Ahmed@users.noreply.github.com>
2024-06-24 16:35:16 +0530

    Add files via upload
    api

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3Y5MzctYWV3LTEzOS5qcGc.jpg",
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
